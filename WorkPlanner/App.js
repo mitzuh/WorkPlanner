9167332741f268846aced70d1c77b087383a8d32
Jimi Savola <jimi.savola@tuni.fi>
2020-01-04 15:58:14 +0200

    Add animations to screen changes

--- WorkPlanner/App.js
+++ WorkPlanner/App.js
@@ -4,29 +4,62 @@ import ProjectsScreen from './ProjectsScreen.js'
 import NewProjectScreen from './NewProjectScreen.js'
 import ProjectInfoScreen from './ProjectInfoScreen.js'
 
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Animated, Easing } from 'react-native';
 
-const MainNavigator = createStackNavigator({
-  MainCalendarScreen: {
-    screen: CalendarScreen,
-    navigationOptions: {
-      header: null,
+const transitionConfig = () => {
+  return {
+    transitionSpec: {
+      duration: 500,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true,
+    },
+    screenInterpolator: sceneProps => {
+      const { layout, position, scene } = sceneProps;
+
+      const thisSceneIndex = scene.index;
+      const width = layout.initWidth;
+
+      const translateX = position.interpolate({
+        inputRange: [thisSceneIndex - 1, thisSceneIndex],
+        outputRange: [width, 0],
+        extrapolate: 'clamp'
+      });
+
+      return {
+        transform: [{ translateX }]
+      }
+    }
+  }
+}
+
+const MainNavigator = createStackNavigator(
+  {
+    MainCalendarScreen: {
+      screen: CalendarScreen,
+      navigationOptions: {
+        header: null,
+      }
+    },
+    SelectedDateScreen: {
+      screen: DayScreen,
+    },
+    ProjectsScreen: {
+      screen: ProjectsScreen,
+    },
+    AddProjectScreen: {
+      screen: NewProjectScreen,
+    },
+    ProjectInfoScreen: {
+      screen: ProjectInfoScreen,
     }
   },
-  SelectedDateScreen: {
-    screen: DayScreen,
-  },
-  ProjectsScreen: {
-    screen: ProjectsScreen,
-  },
-  AddProjectScreen: {
-    screen: NewProjectScreen,
-  },
-  ProjectInfoScreen: {
-    screen: ProjectInfoScreen,
+  {
+    transitionConfig
   }
-});
+);
 
 const App = createAppContainer(MainNavigator);
 
